9e999ff532ddd065cb32afebb47c36ae487af6c8
Francis <fwang36@gmail.com>
2022-02-07 14:17:42 -0500

    added tail

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let result = array.slice(1)
+  return result
+};
+const resultTest = tail([0,2,10]);
+assertEqual(resultTest[0], 2)
