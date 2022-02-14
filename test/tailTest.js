f5148871bba0943902397b327f33550841900a7a
Francis <fwang36@gmail.com>
2022-02-14 13:41:26 -0500

    switched tailTest to mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
+const assert = require('chai').assert
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
 
-const resultTest = tail([0,2,10]);
-assertEqual(resultTest[1], 10)
+
+describe("#tail", () => {
+
+  it("should return [2,10] when given [0,2,10", () => {
+    assert.deepEqual(tail([0,2,10]), [2,10]);
+  });
+
+  it("should return empty when given [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  })
+
+});
