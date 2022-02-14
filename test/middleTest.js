391ce79a18fd93808dbefdb4a4d95b9104e2efbe
Francis <fwang36@gmail.com>
2022-02-14 13:44:58 -0500

    switched middleTest to mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert
 const middle = require('../middle')
 
-assertArraysEqual(middle([3,4,5]),[4])
+describe("#middle", () => {
+
+  it("should return [4] when given [3,4,5]", () => {
+    assert.deepEqual(middle([3,4,5]), [4]);
+  });
+
+  it("should return [4,5] when given [3,4,5,6]", () => {
+    assert.deepEqual(middle([3,4,5,6]), [4,5]);
+  });
+
+});
+
